16bd4da71d38d56ab4ce849d8cf0bd1896480776
Matt Zhang <set.stun@gmail.com>
2023-02-16 00:47:16 -0500

    Added observation visualization notebook

--- lux_entry/wrappers/observations.py
+++ lux_entry/wrappers/observations.py
@@ -1,6 +1,5 @@
 import gym
 from gym import spaces
-from luxai_s2.state.state import ObservationStateDict
 import numpy as np
 from typing import Dict
 
@@ -10,7 +9,7 @@ class ObservationWrapper(gym.ObservationWrapper):
         super().__init__(env)
 
         n_players = 2
-        x, y = self.env.state.env_cfg.map_size // 4, self.env.state.env_cfg.map_size // 4
+        x, y = self.env.state.env_cfg.map_size, self.env.state.env_cfg.map_size
         self.env_cfg = self.env.state.env_cfg
 
         self.observation_space = spaces.Dict({
@@ -65,61 +64,63 @@ class ObservationWrapper(gym.ObservationWrapper):
             # TODO: robot actions
         })
 
-    def observation(self, observation: ObservationStateDict) -> Dict[str, np.ndarray]:
+    def observation(self, observation: Dict) -> Dict[str, np.ndarray]:
         max_factories = self.env_cfg.MAX_FACTORIES
         max_rubble = self.env_cfg.MAX_RUBBLE
         max_lichen = self.env_cfg.MAX_LICHEN_PER_TILE
-        light_cargo_space = self.env_cfg.ROBOTS.LIGHT.CARGO_SPACE
-        light_battery_cap = self.env_cfg.ROBOTS.LIGHT.BATTERY_CAPACITY
-        heavy_cargo_space = self.env_cfg.ROBOTS.HEAVY.CARGO_SPACE
-        heavy_battery_cap = self.env_cfg.ROBOTS.HEAVY.BATTERY_CAPACITY
+        light_cargo_space = self.env_cfg.ROBOTS["LIGHT"].CARGO_SPACE
+        light_battery_cap = self.env_cfg.ROBOTS["LIGHT"].BATTERY_CAPACITY
+        heavy_cargo_space = self.env_cfg.ROBOTS["HEAVY"].CARGO_SPACE
+        heavy_battery_cap = self.env_cfg.ROBOTS["HEAVY"].BATTERY_CAPACITY
         reasonable_factory_cargo_space = 2 * heavy_cargo_space
         ice_water_ratio = self.env_cfg.ICE_WATER_RATIO
         ore_metal_ratio = self.env_cfg.ORE_METAL_RATIO
         reasonable_factory_power = self.env_cfg.FACTORY_CHARGE * 30
         reasonable_max_robots = 50
 
+        observation = observation["player_0"]  # for some reason, observation["player_0"] == observation["player_1"]
         obs = dict()
-        for key, value in self.observation_space.keys():
-            if value.shape[2] > 1:
+        for key, value in self.observation_space.items():
+            if value.shape[1] > 1:
                 obs[key] = (
-                    np.zeros(value.shape)
-                    if value.dtype == spaces.MultiBinary
-                    else np.zeros(value.shape) + value.low
+                    np.zeros(value.shape[1:])
+                    if type(value) == spaces.MultiBinary
+                    else np.zeros(value.shape[1:]) + value.low[0]
                 )
-        obs["has_ice"] = observation.board.ice
-        obs["has_ore"] = observation.board.ore
-        obs["normalized_rubble"] = observation.board.rubble / max_rubble
+        obs["has_ice"] = observation["board"]["ice"]
+        obs["has_ore"] = observation["board"]["ore"]
+        obs["normalized_rubble"] = observation["board"]["rubble"] / max_rubble
         lichen_strains = [[], []]
         for p, player in enumerate(["player_0", "player_1"]):
-            for factory in observation.factories[player].values():
-                lichen_strains[p].append(factory.strain_id)
-                obs["has_factory"][p][factory.pos] = 1
-                obs["normalized_factory_ice"][p][factory.pos] = factory.ice / reasonable_factory_cargo_space
-                obs["normalized_factory_ore"][p][factory.pos] = factory.ore / reasonable_factory_cargo_space
-                obs["normalized_factory_water"][p][factory.pos] = factory.water / (reasonable_factory_cargo_space / ice_water_ratio)
-                obs["normalized_factory_metal"][p][factory.pos] = factory.metal / (reasonable_factory_cargo_space / ore_metal_ratio)
-                obs["normalized_factory_power"][p][factory.pos] = factory.power / reasonable_factory_power
-                obs["total_normalized_factory_ice"][p] += factory.ice / (reasonable_factory_cargo_space * max_factories)
-                obs["total_normalized_factory_ore"][p] += factory.ore / (reasonable_factory_cargo_space * max_factories)
-                obs["total_normalized_factory_water"][p] += factory.water / (reasonable_factory_cargo_space * max_factories / ice_water_ratio)
-                obs["total_normalized_factory_metal"][p] += factory.metal / (reasonable_factory_cargo_space * max_factories / ore_metal_ratio)
-                obs["total_normalized_factory_power"][p] += factory.power / (reasonable_factory_power * max_factories)
-            for robot in observation.units[player].values():
-                obs["has_robot"][p][robot.pos] = 1
-                obs["has_light_robot"][p][robot.pos] = (robot.unit_type == "LIGHT")
-                obs["has_heavy_robot"][p][robot.pos] = (robot.unit_type == "HEAVY")
-                obs["normalized_light_robot_power"][p][robot.pos] = robot.power / light_battery_cap
-                obs["normalized_light_robot_ice"][p][robot.pos] = robot.ice / light_cargo_space
-                obs["normalized_light_robot_ore"][p][robot.pos] = robot.ore / light_cargo_space
-                obs["normalized_heavy_robot_power"][p][robot.pos] = robot.power / heavy_battery_cap
-                obs["normalized_heavy_robot_ice"][p][robot.pos] = robot.ice / heavy_cargo_space
-                obs["normalized_heavy_robot_ore"][p][robot.pos] = robot.ore / heavy_cargo_space
+            for factory in observation["factories"][player].values():
+                lichen_strains[p].append(factory["strain_id"])
+                obs["has_factory"][p][factory["pos"]] = 1
+                obs["normalized_factory_ice"][p][factory["pos"]] = factory["cargo"]["ice"] / reasonable_factory_cargo_space
+                obs["normalized_factory_ore"][p][factory["pos"]] = factory["cargo"]["ore"] / reasonable_factory_cargo_space
+                obs["normalized_factory_water"][p][factory["pos"]] = factory["cargo"]["water"] / (reasonable_factory_cargo_space / ice_water_ratio)
+                obs["normalized_factory_metal"][p][factory["pos"]] = factory["cargo"]["metal"] / (reasonable_factory_cargo_space / ore_metal_ratio)
+                obs["normalized_factory_power"][p][factory["pos"]] = factory["power"] / reasonable_factory_power
+                obs["total_normalized_factory_ice"][p] += factory["cargo"]["ice"] / (reasonable_factory_cargo_space * max_factories)
+                obs["total_normalized_factory_ore"][p] += factory["cargo"]["ore"] / (reasonable_factory_cargo_space * max_factories)
+                obs["total_normalized_factory_water"][p] += factory["cargo"]["water"] / (reasonable_factory_cargo_space * max_factories / ice_water_ratio)
+                obs["total_normalized_factory_metal"][p] += factory["cargo"]["metal"] / (reasonable_factory_cargo_space * max_factories / ore_metal_ratio)
+                obs["total_normalized_factory_power"][p] += factory["power"] / (reasonable_factory_power * max_factories)
+            for robot in observation["units"][player].values():
+                obs["has_robot"][p][robot["pos"]] = 1
+                obs["has_light_robot"][p][robot["pos"]] = (robot["unit_type"] == "LIGHT")
+                obs["has_heavy_robot"][p][robot["pos"]] = (robot["unit_type"] == "HEAVY")
+                obs["normalized_light_robot_power"][p][robot["pos"]] = robot["power"] / light_battery_cap
+                obs["normalized_light_robot_ice"][p][robot["pos"]] = robot["cargo"]["ice"] / light_cargo_space
+                obs["normalized_light_robot_ore"][p][robot["pos"]] = robot["cargo"]["ore"] / light_cargo_space
+                obs["normalized_heavy_robot_power"][p][robot["pos"]] = robot["power"] / heavy_battery_cap
+                obs["normalized_heavy_robot_ice"][p][robot["pos"]] = robot["cargo"]["ice"] / heavy_cargo_space
+                obs["normalized_heavy_robot_ore"][p][robot["pos"]] = robot["cargo"]["ore"] / heavy_cargo_space
                 obs["total_normalized_robots"][p] += 1 / reasonable_max_robots
-                obs["total_normalized_light_robots"][p] += 1/ reasonable_max_robots * (robot.unit_type == "LIGHT")
-                obs["total_normalized_heavy_robots"][p] += 1/ reasonable_max_robots * (robot.unit_type == "HEAVY")
-            obs["has_lichen"][p] = observation.board.lichen_strains in lichen_strains[p]
-            obs["normalized_lichen"][p] = observation.board.lichen / max_lichen * (observation.board.lichen_strains in lichen_strains[p])
-            obs["total_normalized_lichen"][p] = sum(obs["normalized_lichen"][p])
+                obs["total_normalized_light_robots"][p] += 1/ reasonable_max_robots * (robot["unit_type"] == "LIGHT")
+                obs["total_normalized_heavy_robots"][p] += 1/ reasonable_max_robots * (robot["unit_type"] == "HEAVY")
+            obs["has_lichen"][p] = observation["board"]["lichen_strains"] == lichen_strains[p]
+            obs["normalized_lichen"][p] = observation["board"]["lichen"] / max_lichen * (observation["board"]["lichen_strains"] == lichen_strains[p])
+            obs["total_normalized_lichen"][p] = np.sum(obs["normalized_lichen"][p])
+        obs["teams"] = observation["teams"]
 
         return obs
---
+++ lux_entry/wrappers/skip_phases.py
@@ -0,0 +1,74 @@
+import gym
+from luxai_s2.state.state import ObservationStateDict
+import numpy.typing as npt
+from typing import Callable, Dict
+
+from luxai_s2.env import LuxAI_S2
+from luxai_s2.state import ObservationStateDict
+from luxai_s2.unit import BidActionType, FactoryPlacementActionType
+from luxai_s2.utils import my_turn_to_place_factory
+
+from lux_entry.wrappers.controllers import Controller
+
+
+class MainGameOnlyWrapper(gym.Wrapper):
+    def __init__(
+        self,
+        env: LuxAI_S2,
+        bid_policy: Callable[[str, ObservationStateDict], Dict[str, BidActionType]],
+        factory_placement_policy: Callable[[str, ObservationStateDict], Dict[str, FactoryPlacementActionType]],
+        controller: Controller,
+    ) -> None:
+        """
+        Sets the bidding and factory placement policies.
+        """
+        super().__init__(env)
+        self.env = env
+        self.controller = controller
+        self.action_space = controller.action_space
+        self.factory_placement_policy = factory_placement_policy
+        self.bid_policy = bid_policy
+        self.prev_obs = None
+
+    def step(self, action: Dict[str, npt.NDArray]):
+        # here, for each agent in the game we translate their action into a Lux S2 action
+        lux_action = dict()
+        for agent in self.env.agents:
+            if agent in action:
+                lux_action[agent] = self.controller.action_to_lux_action(
+                    agent=agent, obs=self.prev_obs, action=action[agent]
+                )
+            else:
+                lux_action[agent] = dict()
+        
+        # lux_action is now a dict mapping agent name to an action
+        obs, reward, done, info = self.env.step(lux_action)
+        self.prev_obs = obs
+        return obs, reward, done, info
+
+    def reset(self, **kwargs):
+        # we call the original reset function first
+        obs = self.env.reset(**kwargs)
+        
+        # then use the bid policy to go through the bidding phase
+        action = dict()
+        for agent in self.env.agents:
+            action[agent] = self.bid_policy(agent, obs)
+        obs, _, _, _ = self.env.step(action)
+        
+        # while real_env_steps < 0, we are in the factory placement phase
+        # so we use the factory placement policy to step through this
+        while self.env.state.real_env_steps < 0:
+            action = dict()
+            for agent in self.env.agents:
+                if my_turn_to_place_factory(
+                    obs["player_0"]["teams"][agent]["place_first"],
+                    self.env.state.env_steps,
+                ):
+                    action[agent] = self.factory_placement_policy(agent, obs[agent])
+                else:
+                    action[agent] = dict()
+            obs, _, _, _ = self.env.step(action)
+        self.prev_obs = obs
+        
+        return obs
