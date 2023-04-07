01856834fcb67c1184bad6ba3278b50dc9d55571
Matt Zhang <set.stun@gmail.com>
2023-04-06 23:07:09 -0400

    Using probs instead of logits

--- lux_entry/training/model.py
+++ lux_entry/training/model.py
@@ -134,9 +134,8 @@ class JobActionNet(nn.Module):
             self.distant_features(batch_map_features),
         ], dim=1)
         action_logits = self.action_logits(all_features)
-        normalization = action_logits.sum(dim=1, keepdim=True).clamp(min=1e-6)
-        norm_action_logits = action_logits / normalization
-        return norm_action_logits
+        action_probs = torch.softmax(action_logits, dim=1)
+        return action_probs
 
 
 class ActorCriticNet(nn.Module):
@@ -176,27 +175,25 @@ class ActorCriticNet(nn.Module):
             N_ACTIONS,
             *batch_map_features.shape[2:],
         ])
-        robot_action_logits = self.job_action_nets[0](batch_map_features)
+        robot_action_probs = self.job_action_nets[0](batch_map_features)
         """
-        robot_action_logits = torch.stack([
+        robot_action_probs = torch.stack([
             self.job_action_nets[i](batch_map_features)
             if active
             else torch.zeros(action_map_size)
             for i, active in enumerate(self.robot_job_active)
         ], dim=1)
-        robot_action_logits = robot_action_logits * robot_job_probs.unsqueeze(2)
-        robot_action_logits = robot_action_logits.sum(dim=1)
+        robot_action_probs = robot_action_probs * robot_job_probs.unsqueeze(2)
+        robot_action_probs = robot_action_probs.sum(dim=1)
         """
         # find best action for each factory
-        factory_action_logits = self.job_action_nets[-1](batch_map_features)
+        factory_action_probs = self.job_action_nets[-1](batch_map_features)
         # return action logits
-        action_logits = (
-            robot_action_logits * my_robots +
-            factory_action_logits * my_factories
+        action_probs = (
+            robot_action_probs * my_robots +
+            factory_action_probs * my_factories
         )
-        # TODO: normalize action logits correctly
-        # TODO: figure out how to get only unmasked action logits to matter
-        return action_logits
+        return action_probs
 
     def forward_critic(self, batch_map_features: Tensor) -> Tensor:
         return self.value_calculation(batch_map_features)
@@ -231,7 +228,6 @@ class CustomActorCriticPolicy(ActorCriticPolicy):
                     else super().sample()
                 )
                 actions *= unit_mask
-                # TODO: only extract value for single unit position
                 return actions.view(params.shape[0], -1)
 
             def log_prob(self, actions: Tensor) -> Tensor:
@@ -241,12 +237,12 @@ class CustomActorCriticPolicy(ActorCriticPolicy):
                 action_log_probs = log_probs.gather(-1, actions.unsqueeze(-1)).squeeze(-1)
                 action_log_probs *= unit_mask
                 action_log_probs = action_log_probs.sum(dim=(1,2))
-                # TODO: only calculate value for single unit action
                 return action_log_probs
 
-        action_logits = latent_pi.permute(0, 2, 3, 1).contiguous()
-        unit_mask = action_logits.sum(dim=-1) > 0
-        return MyDistribution(logits=action_logits)
+        action_probs = latent_pi.permute(0, 2, 3, 1).contiguous()
+        unit_mask = action_probs.sum(dim=-1) > 0
+        action_probs[..., 0][~unit_mask] = 1  # to make sure prob still adds to 1
+        return MyDistribution(probs=action_probs)
 
 
 def get_model(env: SubprocVecEnv, args: argparse.Namespace) -> PPO:
